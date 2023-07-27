73d04c123fae454614b4c4eaf991c1604b29991d
Rizky Afrianto <rizky.afrianto039@gmail.com>
2023-07-27 18:01:01 +0700

    add db and text

---
+++ my_perpus.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 27, 2023 at 12:09 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my_perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id_admin` int(5) NOT NULL,
+  `nm_admin` varchar(35) NOT NULL,
+  `username` varchar(35) NOT NULL,
+  `password` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id_admin`, `nm_admin`, `username`, `password`) VALUES
+(1, 'admin', 'admin@gmail.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `book_id` int(11) NOT NULL,
+  `book_title` varchar(255) NOT NULL,
+  `category` varchar(100) DEFAULT NULL,
+  `author` varchar(100) DEFAULT NULL,
+  `book_copies` int(11) DEFAULT NULL,
+  `publisher_name` varchar(100) DEFAULT NULL,
+  `is_borrowed` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`book_id`, `book_title`, `category`, `author`, `book_copies`, `publisher_name`, `is_borrowed`) VALUES
+(1, 'Pulang', 'Fantasy', 'Tere Liye', 5000, 'Gramedia', 0),
+(12, 'Meteor', 'Drama', 'Tere Liye', 5000, 'Gramedia', 0),
+(535, 'Harry', 'Fantasy', 'JK', 100, 'Gramedia', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow`
+--
+
+CREATE TABLE `borrow` (
+  `borrow_id` int(11) NOT NULL,
+  `idanggota` int(10) DEFAULT NULL,
+  `date_borrow` date DEFAULT NULL,
+  `due_date` date DEFAULT NULL,
+  `date_return` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrowdetails`
+--
+
+CREATE TABLE `borrowdetails` (
+  `borrowdetail_id` int(11) NOT NULL,
+  `borrow_id` int(11) DEFAULT NULL,
+  `book_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbanggota`
+--
+
+CREATE TABLE `tbanggota` (
+  `idanggota` int(10) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `jeniskelamin` varchar(10) NOT NULL,
+  `alamat` varchar(40) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `foto` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbanggota`
+--
+
+INSERT INTO `tbanggota` (`idanggota`, `nama`, `email`, `jeniskelamin`, `alamat`, `status`, `foto`) VALUES
+(2342, 'hanni', 'rizky@gmail.com', 'Wanita', 'Benteng', 'Tidak Meminjam', '2342.jpg'),
+(21102, 'minji', 'minji@gmail.com', 'Wanita', 'bogor', 'Tidak Meminjam', '21102.jpeg'),
+(21103, 'Hyerin', 'rizky@gmail.com', 'Wanita', 'Benteng', 'Tidak Meminjam', '21103.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`borrow_id`),
+  ADD KEY `idanggota` (`idanggota`);
+
+--
+-- Indexes for table `borrowdetails`
+--
+ALTER TABLE `borrowdetails`
+  ADD PRIMARY KEY (`borrowdetail_id`),
+  ADD KEY `borrow_id` (`borrow_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
+-- Indexes for table `tbanggota`
+--
+ALTER TABLE `tbanggota`
+  ADD PRIMARY KEY (`idanggota`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id_admin` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `borrow`
+--
+ALTER TABLE `borrow`
+  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `borrowdetails`
+--
+ALTER TABLE `borrowdetails`
+  MODIFY `borrowdetail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD CONSTRAINT `borrow_ibfk_1` FOREIGN KEY (`idanggota`) REFERENCES `tbanggota` (`idanggota`);
+
+--
+-- Constraints for table `borrowdetails`
+--
+ALTER TABLE `borrowdetails`
+  ADD CONSTRAINT `borrowdetails_ibfk_1` FOREIGN KEY (`borrow_id`) REFERENCES `borrow` (`borrow_id`),
+  ADD CONSTRAINT `borrowdetails_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
